3f33befe09faf202036d96ff1a0678770e3957b7
linghuazaii <lsh@kloudpeak.com>
2017-03-28 04:54:14 +0000

    m

--- functions/main.cpp
+++ functions/main.cpp
@@ -13,23 +13,21 @@ const char *test_info = NULL;
     } while (0)
 #define END() cout << "============== "<< "END" << " ==============" << endl
 #define TEST2(function, a, b) do {\
+        INFO("TEST "#function);\
         cout << #function << "(" << a << "," << b << ")" <<endl; \
         cout << "result: " << function(a, b) <<endl; \
+        END();\
     } while (0)
 
 
 int main(int argc, char **argv) {
     {
-        INFO("test function max with int");
         int a = 10, b = 20;
         TEST2(Max, a, b);
-        END();
     }
     {
-        INFO("test function max with double");
         double a = 100.999, b = 20.999;
         TEST2(Max, a, b);
-        END();
     }
 
     return 0;
